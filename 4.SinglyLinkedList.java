0c9d5a835965a0e6d4015b120fefbd6bdecf97f8
Ajay Veeraboina <115020401+zeuscodes2003@users.noreply.github.com>
2023-01-10 11:42:13 +0530

    Update 4.SinglyLinkedList.java

--- 4.SinglyLinkedList.java
+++ 4.SinglyLinkedList.java
@@ -5,7 +5,7 @@ class LinkedList {
     private node head;
 
     LinkedList() {
-        head = null;
+        this.head = null;
     }
 
     class node {
@@ -38,11 +38,91 @@ class LinkedList {
 
     }
 
+    public void deleteAtBeg() {
+        if (head == null) {
+            System.out.println("empty");
+            return;
+        }
+        if (head.link == null) {
+            head = null;
+        } else {
+            node ptr = head;
+            head = ptr.link;
+
+        }
+    }
+
+    public void insertany() {
+        int pos, n, i = 1;
+        node ptr = head;
+        System.out.println("enter your position");
+        pos = sc.nextInt();
+
+        System.out.println("enter the value");
+        n = sc.nextInt();
+        node newnode = new node(n);
+        if (pos == 1) {
+            newnode.link = head;
+            head = newnode;
+            return;
+        }
+        while (i < pos) {
+            ptr = ptr.link;
+            i++;
+        }
+        newnode.link = ptr.link;
+        ptr.link = newnode;
+    }
+
+    public void deleteany() {
+        node ptr = head;
+        node ptr1 = null;
+        int i = 1, pos;
+
+        System.out.println("\nenter the position u wish to delete\n");
+        pos = sc.nextInt();
+        if (pos == 1) {
+            head = head.link;
+
+            return;
+        }
+        while (i < pos - 1) {
+            ptr = ptr.link;
+            i++;
+        }
+        ptr1 = ptr.link;
+
+        ptr.link = ptr.link.link;
+
+    }
+
+    public void deleteAtEnd() {
+        node ptr1, ptr;
+        ptr1 = null;
+        ptr = head;
+        if (head == null) {
+            System.out.println("empty for deletion");
+            return;
+        }
+        if (head.link == null) {
+            head = null;
+            return;
+        }
+
+        while (ptr.link != null) {
+            ptr1 = ptr;
+            ptr = ptr.link;
+        }
+        ptr1.link = null;
+
+    }
+
     public static void main(String[] args) {
         int ch;
         LinkedList obj = new LinkedList();
         while (true) {
-            System.out.println("MENU\n1:insert\n2:display\n");
+            System.out.println(
+                    "MENU\n1:insert\n2:display\n3:deleteatbeg\n4:deleteatend\n5:insertany\n6:deleteant\n7:exit()");
             System.out.println("enter your choice");
             ch = sc.nextInt();
             switch (ch) {
@@ -52,12 +132,24 @@ class LinkedList {
                 case 2:
                     obj.display();
                     break;
-                default:
+                case 3:
+                    obj.deleteAtBeg();
+                    break;
+                case 4:
+                    obj.deleteAtEnd();
+                    break;
+                case 5:
+                    obj.insertany();
+                    break;
+                case 6:
+                    obj.deleteany();
+                    break;
+                case 7:
                     break;
             }
 
         }
 
     }
+
 }
-//JAVA
